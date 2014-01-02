54de16dfb2ffa107c1b873677ee18f035277a684
jrietema <jan.rietema@web.de>
2014-01-02 20:26:58 +0100

    fancytree page navigation implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
     html = label_tag(name, checkbox, :class => "checkbox#{checked ? ' checked' : ''}")
     html + hidden_field_tag(name, (value.to_i == 0) ? '1' : '0', :id => nil)
   end
+
+  def fancytree_pages_hash(pages, branch_id=nil)
+    pages[branch_id].map do |page|
+      child_pages = pages[page.id] || []
+      page_hash = { title: page.label, key: page.id, href: edit_admin_cms_site_page_path(site_id: @site.id, id: page.id, format: :js) }
+      unless child_pages.empty?
+        page_hash.merge!({ folder: true, children: fancytree_pages_hash(pages, page.id)})
+      end
+      page_hash
+    end
+  end
 end
