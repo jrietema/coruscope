6b62ee04065f24925d21e67ce8726724872d1457
jrietema <jan.rietema@web.de>
2014-01-13 19:29:03 +0100

    added SMINT page menu generation and offset of fixed position

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,17 +1,21 @@
 /* Inserts Anchors and Link Elements for internal page navigation */
 
-function initPageNav(list, selector) {
+function initPageNav(list, selector, offset) {
     var idx = 0;
+    var navId = $(list).parent('div, nav').attr('id');
     $(selector).each(function() {
        var name = $(this).html().trim();
        if(name == '') { return; }
        var id = idx++;
        var newLink = document.createElement('a');
        $(newLink).attr('href', '#section' + id);
+       $(newLink).attr('id', 'goto-section' + id);
        newLink.innerHTML = name;
        var listItem = document.createElement('li');
        $(listItem).append(newLink);
        $(list).append(listItem);
        $(this).attr('id', 'section' + id);
+       $(this).addClass('goto-section' + id);
     });
+    $('#' + navId).smint({verticalOffset: offset || 0});
 }
