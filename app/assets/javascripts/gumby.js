f71f6eb6150d54d90f6b33c110f9792788c40d08
jrietema <jan.rietema@web.de>
2014-03-10 16:38:01 +0100

    update gumby sources and include mixins needed for functional responsive nav

--- app/assets/javascripts/gumby.js
+++ app/assets/javascripts/gumby.js
@@ -1,3 +1,11 @@
 /* Gumby Framework JS */
 //= require gumby/libs/modernizr-2.6.2.min
 //= require gumby/libs/gumby
+//= require gumby/libs/ui/gumby.checkbox
+//= require gumby/libs/ui/gumby.radiobtn
+//= require gumby/libs/ui/gumby.fixed
+//= require gumby/libs/ui/gumby.navbar
+//= require gumby/libs/ui/gumby.toggleswitch
+//= require gumby/libs/ui/jquery.validation
+//= require gumby/libs/extensions/gumby.fittext
+//= require gumby/libs/gumby.init
